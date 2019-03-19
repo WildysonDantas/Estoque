e3df3d2569d27eef4f26f4844a4a22c64cf02729
Wildyson Dantas <wildyson.santos@gmail.com>
2019-03-19 15:25:48 -0300

    Update settings.py

--- src/API/API/settings.py
+++ src/API/API/settings.py
@@ -90,11 +90,11 @@ INTERNAL_IPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['POSTGRESQL_ADDON_DB'],
-        'USER': os.environ['POSTGRESQL_ADDON_USER'],
-        'PASSWORD': os.environ['POSTGRESQL_ADDON_PASSWORD'],
-        'HOST': os.environ['POSTGRESQL_ADDON_HOST'],
-        'PORT': os.environ['POSTGRESQL_ADDON_PORT']
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT']
         
     }
 }
