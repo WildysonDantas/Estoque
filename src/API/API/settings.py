0d00c162a45c6697ebc72e9c77dad8b5d2fa6473
Wildyson Dantas <wildyson.santos@gmail.com>
2019-03-20 03:03:52 -0300

    Update settings.py

--- src/API/API/settings.py
+++ src/API/API/settings.py
@@ -60,10 +60,10 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-      'rest_framework.authentication.BasicAuthentication',
+      'rest_framework.authentication.TokenAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny'
+         'rest_framework.permissions.IsAuthenticated',
    )
 }
 
